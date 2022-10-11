efb73f43473907098ab84b57eadb17010be69213
jaipalkomakula <jaipalkomakula@gmail.com>
2022-10-11 03:19:51 +0000

    add

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# servicemesh
+# ServiceMesh
+
+## Service Mesh
+
+
+
+
+
+
+
---
+++ app/flask/flask-v2/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/v1')
+def login():
+    return render_template('v2.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
